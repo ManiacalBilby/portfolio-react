7f30d379a125d01b40971a76d1000f36068d8327
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-02-02 13:59:05 -0500

    copying html and css from other portfolio app

--- src/App.js
+++ src/App.js
@@ -5,15 +5,41 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+<body>
+
+    <header>
+        <h1>Tyler McDonald</h1>
+    </header>
+
+    <main>
+        <p>I am a full-stack developer currently enrolled in the Web Development Immersive program at General Assembly in Atlanta,
+            GA. I have a background in childcare and addiction counseling and made a decision to pursue a field where I could
+            put my love of problem solving to work</p>
+        <p>While I am still discovering what type of work I would like to do, a positive work environment is exremely important
+            to me. Feel free to review my projects below and interact with me via the social sites below.</p>
+        <div>
+            <h3>Projects</h3>
+            <div>
+                <a href="http://twmcdonaldhangman.bitballoon.com/">Hangman Game</a>
+            </div>
+            <div>
+                <a href="https://lit-river-55055.herokuapp.com">Sticktimes</a>
+            </div>
+            <div>
+                <a href="https://young-bastion-12479.herokuapp.com/">TreeTime</a>
+            </div>
+        </div>
+        <div>
+            <h3>Social</h3>
+            <a href="https://twitter.com/tylermcdonald">Twitter</a>
+            <a href="https://github.com/ManiacalBilby">Github</a>
+            <a href="https://www.linkedin.com/in/tylerwmcdonald/">Linkedin</a>
+            <a href="https://young-bastion-12479.herokuapp.com/"></a>
+        </div>
+    </main>
+
+    <script src="app.js"></script>
+</body>
     );
   }
 }
