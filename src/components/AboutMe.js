d5c332146056d64a30b4a2a2c787d0f6a3daafc7
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-03-14 11:23:05 -0400

    Font adjustment

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  font-size: 1.25rem;
+  font-size: 1rem;
   padding: 0 0 10px 10px;
   background-image: url("https://i.imgur.com/spM38v9.jpg");
   background-size: cover;
