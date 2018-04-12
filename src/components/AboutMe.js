31ab477a774917dc56c5c16ed5f69b416e8ed5a0
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-04-12 11:46:44 -0400

    Changed background image and placement. Refactored code for display of paragraphs.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react';
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  /* flex-grow: 1; */
   display: flex;
   flex-direction: column;
   justify-content: center;
-  font-size: .75rem;
-  padding: 0 0 10px 10px;
-  background-image: url("https://i.imgur.com/spM38v9.jpg");
+  text-align: center;
+  font-size: 1rem;
+  background-image: url("https://i.imgur.com/aLr5Jn2.jpg");
   background-size: cover;
-  background-position: bottom;
+  background-position: center;
   background-attachment: fixed;
-  color: white;
+  color: ghostwhite;
   text-shadow: 2px 2px 5px rgba(7, 7, 7, 1);
-  min-height: 100px;
   @media (min-width: 768px) and (min-height: 450px) {
     font-size: 1.5rem;
   };
@@ -22,18 +20,16 @@ const Wrapper = styled.div`
     font-size: 1.75rem;
   };
   `
-
-  const SecondParagraph = styled.p`
-    margin-top: 0;
-    /* padding-bottom: 5px; */
+  const AboutP = styled.p`
+    margin: 8px auto;
   `
 
 class AboutMe extends Component {
   render() {
     return (
       <Wrapper>
-        <p>I am a full-stack developer who enjoys a collaborative work environment while solving problems small and large with clean, concise code.</p> 
-        <SecondParagraph>Having worked in fields strongly relying on my education in psychology, I bring an understanding of human behavior and interactions that is valuable when working with teams and clients.</SecondParagraph>
+        <AboutP>I am a full-stack developer who enjoys a collaborative work environment while solving problems small and large with clean, concise code.</AboutP> 
+        <AboutP>Having worked in fields strongly relying on my education in psychology, I bring an understanding of human behavior and interactions that is valuable when working with teams and clients.</AboutP>
       </Wrapper>
     );
   }
